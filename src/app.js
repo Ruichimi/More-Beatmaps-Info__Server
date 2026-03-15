b1683032f7ef3f5f04b55443b04dc231d4457f7f
Ruichimi <thekeykeyloy@gmail.com>
2026-03-15 22:26:10 +0100

    Refactor app and server into separate files

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+require('dotenv').config();
+require('module-alias/register');
+
+const app = express();
+const routes = require('./routes');
+const { initCommands } = require('$/commands/ServerRunningCommandsInterface');
+const requestLogger = require('./middlewares/requestsLogger');
+const errorMiddleware = require('./middlewares/error');
+
+const serverDir = path.resolve();
+
+app.use(express.static(path.join(serverDir, 'public')));
+app.set('trust proxy', 1);
+app.use(express.json({ limit: '320kb' }));
+app.use(requestLogger);
+
+//Send index.html for all routes except api
+app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(serverDir, 'public', 'index.html'))
+});
+
+global.getTime = () => {
+    const now = new Date();
+    return now.toTimeString().split(' ')[0];
+};
+
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: "Authorization, Content-Type, x-client-id, x-retry-request"
+};
+
+app.use(cors(corsOptions));
+app.use(routes);
+
+app.use(errorMiddleware);
+
+initCommands();
+
+module.exports = app;
