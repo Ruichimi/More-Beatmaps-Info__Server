bfea6fb31054a36981755f50218de125488e47c4
Ruichimi <thekeykeyloy@gmail.com>
2026-03-20 21:56:41 +0100

    Improve error handling

--- src/errors/AppError.js
+++ src/errors/AppError.js
@@ -1,11 +1,12 @@
 class AppError extends Error {
-    constructor(message, statusCode = 500, code, details = null) {
-        super(message);
+    constructor(message, statusCode = 500, code = 'UNKNOWN_ERROR', details = null, cause = null) {
+        super(message, { cause });
 
         this.name = this.constructor.name;
         this.statusCode = statusCode;
         this.code = code;
         this.details = details;
+        this.isOperational = true;
 
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, this.constructor);
--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,13 +1,15 @@
+const AppError = require('$/errors/AppError');
+
 module.exports = (err, req, res, next) => {
-    if (err.statusCode) {
+    console.error(err);
+
+    if (err instanceof AppError) {
         return res.status(err.statusCode).json({
             error: err.message,
             code: err.code,
             details: err.details
         });
     }
-
-    console.error(err.stack);
 
     res.status(500).json({
         error: 'Internal server error'
